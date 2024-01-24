1da275edbd22bc64e624c3d518bbe5cef2777995
OluwatoyinAguiyi <38816490+OluwatoyinAguiyi@users.noreply.github.com>
2024-01-24 00:21:08 +0000

    Configures docker compose to add checks to wait for db

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,39 @@
+"""
+Test custom Django managemant commands
+"""
+
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test Commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Tedt waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError"""
+        # Syntax below tells the mock what to return each time the patch
+        # is called
+        # First two times its called, raise Psycopg2Error
+        # Next 3 times its called, raise OperationalError
+        # 6th time its called, return True
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
