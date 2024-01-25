f2f571e5510afd36cfeaae3d4aec5c8065ba43fc
OluwatoyinAguiyi <38816490+OluwatoyinAguiyi@users.noreply.github.com>
2024-01-25 13:16:33 +0000

    fixing lint error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,7 +34,7 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test that creating a user without an email address raises a ValueError"""
+        """Test creating a user without an email address raises a ValueError"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
